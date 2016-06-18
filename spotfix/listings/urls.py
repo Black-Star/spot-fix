b7305c05e9a6ef176f84f2dd188f124d388fbbd5
ravi_teja_n <ravi@quantumgraph.com>
2016-06-18 15:14:05 +0530

    updating spotfixes

--- spotfix/listings/urls.py
+++ spotfix/listings/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
-from listings import views
-urlpatterns = [
-    url(r'^spot-fix/$', views.SpotFixList.as_view()),
-    url(r'^$', views.index),
-]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+from listings.views import SpotFixList
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+router.register(r'spot-fix', SpotFixList)
+
+urlpatterns = router.urls
--- spotfix/listings/views.py
+++ spotfix/listings/views.py
@@ -3,15 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.decorators import authentication_classes
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from rest_framework import viewsets
 
 from listings.serializers import SpotFixSerializer
 from listings.models import SpotFix
+from rest_framework import pagination
 
 
-@authentication_classes((TokenAuthentication,))
-class SpotFixList(generics.RetrieveAPIView):
+class StandardResultsSetPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 12
+
+
+class SpotFixList(viewsets.ModelViewSet):
     queryset = SpotFix.objects.all()
     serializer_class = SpotFixSerializer
+    pagination_class = StandardResultsSetPagination
 
 
 def index(request):
